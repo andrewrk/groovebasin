andrewrk/groovebasin

commit 415ae5a7576b914f3c15b0aac70a3793b3ed1ead

client: fix player prediction when currently playing track is removed
closes #327

src/client/playerclient.js
@@ -562,20 +562,22 @@ PlayerClient.prototype.shiftIds = function(trackIdSet, offset) {
 
 PlayerClient.prototype.removeIds = function(trackIds){
   if (trackIds.length === 0) return;
-  var ids = [];
+
+  var currentId = this.currentItem && this.currentItem.id;
+  var currentIndex = this.currentItem && this.currentItem.index;
   for (var i = 0; i < trackIds.length; i += 1) {
     var trackId = trackIds[i];
-    var currentId = this.currentItem && this.currentItem.id;
-    if (currentId === trackId) {
-      this.currentItemId = null;
-      this.currentItem = null;
+    if (trackId === currentId) {
+      this.trackStartDate = new Date();
+      this.pausedTime = 0;
     }
-    ids.push(trackId);
-    var item = this.queue.itemTable[trackId];
-    delete this.queue.itemTable[item.id];
-    this.refreshPlaylistList(this.queue);
+    delete this.queue.itemTable[trackId];
   }
-  this.sendCommand('remove', ids);
+  this.refreshPlaylistList(this.queue);
+  this.currentItem = (currentIndex == null) ? null : this.queue.itemList[currentIndex];
+  this.currentItemId = this.currentItem && this.currentItem.id;
+
+  this.sendCommand('remove', trackIds);
   this.emit('queueUpdate');
 };
 

commit 2d48fc7ed5bb9eba1f5918585ed9673726029514

client: fix player prediction regression from 415ae5a7

src/client/playerclient.js
@@ -565,14 +565,20 @@ PlayerClient.prototype.removeIds = function(trackIds){
 
   var currentId = this.currentItem && this.currentItem.id;
   var currentIndex = this.currentItem && this.currentItem.index;
+  var offset = 0;
   for (var i = 0; i < trackIds.length; i += 1) {
     var trackId = trackIds[i];
     if (trackId === currentId) {
       this.trackStartDate = new Date();
       this.pausedTime = 0;
     }
+    var item = this.queue.itemTable[trackId];
+    if (item.index < currentIndex) {
+      offset -= 1;
+    }
     delete this.queue.itemTable[trackId];
   }
+  currentIndex += offset;
   this.refreshPlaylistList(this.queue);
   this.currentItem = (currentIndex == null) ? null : this.queue.itemList[currentIndex];
   this.currentItemId = this.currentItem && this.currentItem.id;
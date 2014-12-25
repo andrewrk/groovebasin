andrewrk/groovebasin

commit f56dd50b406359b5fc639d3ab235d599c2410a90

better rescan behavior
rescan deletes mtimes in db instead of relying on a flag in memory.
This way if you kill the server in the middle of a rescan, it picks up
where it left off.

closes #269

lib/player.js
@@ -638,18 +638,27 @@ Player.prototype.detachEncoder = function(cb) {
   }
 };
 
-Player.prototype.requestUpdateDb = function(dirName, forceRescan, cb) {
+Player.prototype.deleteDbMtimes = function(cb) {
+  cb = cb || logIfDbError;
+  var updateCmds = [];
+  for (var key in this.libraryIndex.trackTable) {
+    var dbFile = this.libraryIndex.trackTable[key];
+    delete dbFile.mtime;
+    persistDbFile(dbFile, updateCmds);
+  }
+  this.db.batch(updateCmds, cb);
+};
+
+Player.prototype.requestUpdateDb = function(dirName, cb) {
   var fullPath = path.resolve(this.musicDirectory, dirName || "");
   this.dirScanQueue.add(fullPath, {
     dir: fullPath,
-    forceRescan: forceRescan,
   }, cb);
 };
 
 Player.prototype.refreshFilesIndex = function(args, cb) {
   var self = this;
   var dir = args.dir;
-  var forceRescan = args.forceRescan;
   var dirWithSlash = ensureSep(dir);
   var walker = findit(dirWithSlash, {followSymlinks: true});
   var thisScanId = uuid();
@@ -679,7 +688,7 @@ Player.prototype.refreshFilesIndex = function(args, cb) {
     var dirEntry = self.getOrCreateDir(dirName);
     dirEntry.entries[baseName] = thisScanId;
     var fileMtime = stat.mtime.getTime();
-    onAddOrChange(self, relPath, fileMtime, forceRescan);
+    onAddOrChange(self, relPath, fileMtime);
   });
   walker.on('error', function(err) {
     walker.stop();
@@ -1219,11 +1228,7 @@ Player.prototype.persistDirEntry = function(dirEntry, cb) {
 Player.prototype.persistDbFile = function(dbFile, updateCmds) {
   this.libraryIndex.addTrack(dbFile);
   this.dbFilesByPath[dbFile.file] = dbFile;
-  updateCmds.push({
-    type: 'put',
-    key: LIBRARY_KEY_PREFIX + dbFile.key,
-    value: serializeFileData(dbFile),
-  });
+  persistDbFile(dbFile, updateCmds);
 };
 
 Player.prototype.persistOneDbFile = function(dbFile, cb) {
@@ -2178,6 +2183,14 @@ Player.prototype.getDefaultQueuePosition = function() {
   };
 };
 
+function persistDbFile(dbFile, updateCmds) {
+  updateCmds.push({
+    type: 'put',
+    key: LIBRARY_KEY_PREFIX + dbFile.key,
+    value: serializeFileData(dbFile),
+  });
+}
+
 function persistQueueItem(item, updateCmds) {
   updateCmds.push({
     type: 'put',
@@ -2186,13 +2199,13 @@ function persistQueueItem(item, updateCmds) {
   });
 }
 
-function onAddOrChange(self, relPath, fileMtime, forceRescan, cb) {
+function onAddOrChange(self, relPath, fileMtime, cb) {
   cb = cb || logIfError;
 
   // check the mtime against the mtime of the same file in the db
   var dbFile = self.dbFilesByPath[relPath];
 
-  if (dbFile && !forceRescan) {
+  if (dbFile) {
     var dbMtime = dbFile.mtime;
 
     if (dbMtime >= fileMtime) {

lib/mpd_protocol.js
@@ -1722,7 +1722,10 @@ function handleUpdate(self, args, forceRescan, cb) {
     cb(ERR_CODE_ARG, "Malformed path");
     return;
   }
-  self.player.requestUpdateDb(dirEntry.dirName, forceRescan);
+  if (forceRescan) {
+    self.player.deleteDbMtimes();
+  }
+  self.player.requestUpdateDb(dirEntry.dirName);
   self.push("updating_db: 1\n");
   cb();
 }

commit eaab49d78947f6ee59df3944f920dc84f9950650

Fixed uploads.
Was broken due to change in player.onAddOrChange introduced in
f56dd50b406359b5fc639d3ab235d599c2410a90 that wasn't reflected
in all callsites.

lib/player.js
@@ -2820,7 +2820,7 @@ function importFileAsSong(self, srcFullPath, filenameHintWithoutPath, cb) {
           }
           return;
         }
-        onAddOrChange(self, destRelPath, (new Date()).getTime(), true, function(err, dbFile) {
+        onAddOrChange(self, destRelPath, (new Date()).getTime(), function(err, dbFile) {
           if (err) return cb(err);
           newDbFile = dbFile;
           cb();
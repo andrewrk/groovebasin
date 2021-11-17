0313e3fc0a15a46436514ddbe6314eafd3f0707b
Josh Wolfe <thejoshwolfe@gmail.com>
2021-11-17 04:54:51 +0000

    retry connecting to the server

--- src/client/zig/client_main.zig
+++ src/client/zig/client_main.zig
@@ -1,22 +1,18 @@
-comptime {
-    _ = @import("callback.zig");
-}
+const std = @import("std");
 const websocket_handler = @import("websocket_handler.zig");
 const browser = @import("browser.zig");
+const env = @import("browser_env.zig");
 const ui = @import("groovebasin_ui.zig");
 
 export fn main() void {
     browser.print("zig: hello world");
     ui.loadDomElements();
 
-    browser.openWebSocket(
-        &websocket_handler.onOpen,
-        undefined,
-        &websocket_handler.onClose,
-        undefined,
-        &websocket_handler.onError,
-        undefined,
-        &websocket_handler.onMessage,
-        undefined,
-    );
+    websocket_handler.open();
+}
+
+pub fn panic(msg: []const u8, stacktrace: ?*std.builtin.StackTrace) noreturn {
+    _ = stacktrace;
+    env.panic(msg.ptr, msg.len);
+    unreachable;
 }
--- src/client/zig/dom.zig
+++ src/client/zig/dom.zig
@@ -1,13 +1,13 @@
-const browser = @import("./browser_env.zig");
+const env = @import("./browser_env.zig");
 
 pub fn getElementById(id: []const u8) i32 {
-    return browser.getElementById(id.ptr, id.len);
+    return env.getElementById(id.ptr, id.len);
 }
 
 pub fn setShown(handle: i32, shown: bool) void {
-    browser.setElementShown(handle, @boolToInt(shown));
+    env.setElementShown(handle, @boolToInt(shown));
 }
 
 pub fn setTextContent(handle: i32, text: []const u8) void {
-    browser.setElementTextContent(handle, text.ptr, text.len);
+    env.setElementTextContent(handle, text.ptr, text.len);
 }

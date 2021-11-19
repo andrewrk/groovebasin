c52d4cd6437f36482b7fb9a5cb34133ec51c8590
Josh Wolfe <thejoshwolfe@gmail.com>
2021-11-19 17:18:48 +0000

    use async await in the client. doesn't work

--- src/client/zig/websocket_handler.zig
+++ src/client/zig/websocket_handler.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const json = std.json;
 
 const browser = @import("browser.zig");
 const env = @import("browser_env.zig");
@@ -22,19 +21,22 @@ pub fn open() void {
     std.debug.assert(loading_state == .none);
     loading_state = .connecting;
     env.openWebSocket(
-        &onOpen,
+        &onOpenCallback,
         undefined,
-        &onClose,
+        &onCloseCallback,
         undefined,
-        &onError,
+        &onErrorCallback,
         undefined,
-        &onMessage,
+        &onMessageCallback,
         undefined,
     );
 }
 
-fn onOpen(context: *callback.Context, handle: i32) void {
+fn onOpenCallback(context: *callback.Context, handle: i32) void {
     _ = context;
+    _ = async onOpen(handle);
+}
+fn onOpen(handle: i32) void {
     browser.print("zig: websocket opened");
 
     std.debug.assert(loading_state == .connecting);
@@ -43,27 +45,87 @@ fn onOpen(context: *callback.Context, handle: i32) void {
     ui.setLoadingState(.good);
 
     periodic_ping_handle = env.setInterval(&periodicPingCallback, undefined, periodic_ping_interval_ms);
-    periodicPing();
+    periodicPingAndCatch();
 }
 
-fn writeRequest(request: protocol.Request) !void {
-    var out_buffer: [0x1000]u8 = undefined;
-    var fixed_buffer_stream = std.io.fixedBufferStream(&out_buffer);
-    const out_stream = fixed_buffer_stream.writer();
-    try json.stringify(request, json.StringifyOptions{}, out_stream);
-
-    sendMessage(fixed_buffer_stream.getWritten());
+var next_seq_id: u32 = 0;
+fn generateSeqId() u32 {
+    defer {
+        next_seq_id +%= 1;
+    }
+    return next_seq_id;
 }
 
-fn onClose(context: *callback.Context, code: i32) void {
+const ResponseHandler = struct {
+    frame: anyframe,
+    response_buf_ptr: *[]u8,
+};
+var pending_requests: std.AutoHashMapUnmanaged(u32, ResponseHandler) = .{};
+
+const Call = struct {
+    seq_id: u32,
+    request: std.ArrayList(u8),
+    response_buf: []u8,
+    response: std.io.FixedBufferStream([]u8),
+    pub fn init(opcode: protocol.Opcode) !@This() {
+        var self = @This(){
+            .seq_id = generateSeqId(),
+            .request = std.ArrayList(u8).init(gpa),
+            .response_buf = undefined,
+            .response = undefined,
+        };
+
+        // write the request header.
+        try self.request.writer().writeStruct(protocol.RequestHeader{
+            .seq_id = self.seq_id,
+            .op = opcode,
+        });
+
+        return self;
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.request.deinit();
+        self.response.deinit();
+    }
+
+    pub fn writer(self: *@This()) std.ArrayList(u8).Writer {
+        return self.request.writer();
+    }
+
+    pub fn reader(self: *@This()) std.io.FixedBufferStream([]u8).Reader {
+        return self.response.reader();
+    }
+
+    pub fn send(self: *@This()) !void {
+        const buffer = self.request.items;
+        suspend {
+            try pending_requests.put(gpa, self.seq_id, .{
+                .frame = @frame(),
+                .response_buf_ptr = &self.response_buf,
+            });
+            env.sendMessage(websocket_handle, buffer.ptr, buffer.len);
+        }
+        // we now have a response buf.
+        self.response = std.io.fixedBufferStream(self.response_buf);
+    }
+};
+
+fn onCloseCallback(context: *callback.Context, code: i32) void {
     _ = context;
+    _ = async onClose(code);
+}
+fn onClose(code: i32) void {
     _ = code;
     browser.print("zig: websocket closed");
     handleNoConnection();
 }
 
-fn onError(context: *callback.Context) void {
+fn onErrorCallback(context: *callback.Context) void {
     _ = context;
+    _ = async onError();
+}
+fn onError() void {
     browser.print("zig: websocket error");
     handleNoConnection();
 }
@@ -71,36 +133,31 @@ fn onError(context: *callback.Context) void {
 var gpa_state: std.heap.GeneralPurposeAllocator(.{}) = .{};
 const gpa = &gpa_state.allocator;
 
-fn onMessage(context: *callback.Context, handle: i32, _len: i32) void {
+fn onMessageCallback(context: *callback.Context, handle: i32, _len: i32) void {
     _ = context;
+    _ = async onMessage(handle, _len);
+}
+fn onMessage(handle: i32, _len: i32) void {
     const len = @intCast(usize, _len);
 
-    var buffer: [0x1000]u8 = undefined;
-    browser.readBlob(handle, buffer[0..len]);
-
-    browser.print(buffer[0..len]);
-
-    const response = json.parse(protocol.Response, &json.TokenStream.init(buffer[0..len]), json.ParseOptions{ .allocator = gpa }) catch |err| {
+    const buffer = gpa.alloc(u8, len) catch |err| {
         @panic(@errorName(err));
     };
-    switch (response.data) {
-        .ping => |server_time| {
-            const client_time = browser.getTime();
-            const client_ns: i128 = @as(i128, client_time.s) * 1_000_000_000 + client_time.ns;
-            const server_ns: i128 = @as(i128, server_time.s) * 1_000_000_000 + server_time.ns;
-            const lag_ns = client_ns - server_ns;
-            ui.setLag(lag_ns);
-        },
-        .query => |query_response| {
-            if (query_response.new_library) |library| {
-                ui.setLibrary(library);
-            }
-        },
-    }
-}
+    browser.readBlob(handle, buffer);
 
-pub fn sendMessage(message: []const u8) void {
-    env.sendMessage(websocket_handle, message.ptr, message.len);
+    browser.print(buffer);
+
+    var stream = std.io.fixedBufferStream(buffer);
+    const reader = stream.reader();
+    const header = reader.readStruct(protocol.ResponseHeader) catch |err| {
+        @panic(@errorName(err));
+    };
+    const handler = (pending_requests.fetchRemove(header.seq_id) orelse {
+        @panic("received a response for unrecognized seq_id");
+    }).value;
+
+    handler.response_buf_ptr.* = buffer;
+    resume handler.frame;
 }
 
 const retry_timeout_ms = 1000;
@@ -110,11 +167,14 @@ fn handleNoConnection() void {
     ui.setLoadingState(.no_connection);
 
     env.clearTimer(periodic_ping_handle.?);
-    _ = env.setTimeout(&retryOpen, undefined, retry_timeout_ms);
+    _ = env.setTimeout(&retryOpenCallback, undefined, retry_timeout_ms);
 }
 
-fn retryOpen(context: *callback.Context) void {
+fn retryOpenCallback(context: *callback.Context) void {
     _ = context;
+    _ = async retryOpen();
+}
+fn retryOpen() void {
     if (loading_state != .backoff) return;
     loading_state = .none;
     open();
@@ -124,25 +184,33 @@ var periodic_ping_handle: ?i64 = null;
 const periodic_ping_interval_ms = 10_000;
 fn periodicPingCallback(context: *callback.Context) void {
     _ = context;
-    periodicPing();
+    _ = async periodicPingAndCatch();
 }
-fn periodicPing() void {
-    const request = protocol.Request{
-        .seq = 123,
-        .op = .ping,
-    };
-    writeRequest(request) catch |err| {
+fn periodicPingAndCatch() void {
+    periodicPing() catch |err| {
         @panic(@errorName(err));
     };
+}
+fn periodicPing() !void {
+    {
+        var ping_call = try Call.init(.ping);
+        try ping_call.send();
+
+        const milliseconds = env.getTime();
+        const client_ns = @as(i128, milliseconds) * 1_000_000;
+        const server_ns = try ping_call.reader().readIntLittle(i128);
+        const lag_ns = client_ns - server_ns;
+        ui.setLag(lag_ns);
+    }
 
     // Also by the way, let's query for the data or something.
-    writeRequest(protocol.Request{
-        .seq = 1223,
-        .op = .query,
-        .data = protocol.QueryRequest{
-            .last_library = null,
-        },
-    }) catch |err| {
-        @panic(@errorName(err));
-    };
+    {
+        var query_call = try Call.init(.query);
+        try query_call.writer().writeStruct(protocol.QueryRequest{
+            .last_library = 0,
+        });
+        try query_call.send();
+
+        // TODO: read the response.
+    }
 }
--- src/shared/protocol.zig
+++ src/shared/protocol.zig
@@ -1,57 +1,26 @@
-const json = @import("std").json;
-
-pub const Request = struct {
+pub const RequestHeader = extern struct {
     // An arbitrary number that will be included in the corresponding Response.
-    seq: u32,
+    seq_id: u32,
     op: Opcode,
-
-    data: ?QueryRequest = null,
 };
 
-pub const Opcode = enum {
-    ping,
-    query,
-
-    pub fn jsonStringify(self: @This(), options: json.StringifyOptions, out_stream: anytype) @TypeOf(out_stream).Error!void {
-        try json.stringify(@tagName(self), options, out_stream);
-    }
+pub const Opcode = enum(u8) {
+    ping = 0,
+    query = 1,
 };
 
-pub const Response = struct {
-    // The seq of the corresponding Request.
-    seq: u32,
-
-    data: ResponseData,
+pub const ResponseHeader = extern struct {
+    // The seq_id of the corresponding Request.
+    seq_id: u32,
 };
 
-pub const ResponseData = union(enum) {
-    ping: Timestamp,
-    query: QueryResponse,
+pub const Track = extern struct {
+    file_path: u32,
+    title: u32,
+    artist: u32,
+    album: u32,
 };
 
-pub const Timestamp = struct {
-    /// Seconds of UTC time since Unix epoch 1970-01-01T00:00:00Z.
-    s: i64,
-    /// Nanoseconds in the range 0...999_999_999 inclusive.
-    ns: i32,
-};
-
-pub const QueryRequest = struct {
-    last_library: ?u32,
-};
-
-pub const QueryResponse = struct {
-    new_library: ?Library,
-};
-
-pub const Library = struct {
-    version: u32,
-    tracks: []const Track,
-};
-
-pub const Track = struct {
-    id: u64,
-    title: []const u8,
-    artist: []const u8,
-    album: []const u8,
+pub const QueryRequest = extern struct {
+    last_library: u64,
 };

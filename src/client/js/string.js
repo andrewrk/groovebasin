fbae995b603a1657366aced4ddc9e43d2a114dea
Josh Wolfe <thejoshwolfe@gmail.com>
2021-11-21 00:18:08 +0000

    library items can be clicked. nothing happens

--- src/client/js/string.js
+++ src/client/js/string.js
@@ -6,6 +6,14 @@ function decodeString(ptr, len) {
     return decoder.decode(new Uint8Array(wasmExports.memory.buffer, ptr, len));
 }
 
+const encoder = new TextEncoder();
+function encodeString(s, dest) {
+    const {read, written} = encoder.encodeInto(s, dest);
+    if (read < s.length) throw new Error("dest too small");
+    if (written < dest.length) throw new Error("dest too large");
+}
+
 return {
     decodeString,
+    encodeString,
 };

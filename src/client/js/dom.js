10d08af096d2b49690a22dd77248ed4b3338423a
Josh Wolfe <thejoshwolfe@gmail.com>
2021-11-29 00:27:19 +0000

    don't even need a garbage collector for the dom registry

--- src/client/js/dom.js
+++ src/client/js/dom.js
@@ -4,78 +4,93 @@ const {HandleRegistry} = require("handleRegistry");
 const elementRegistry = new HandleRegistry();
 const eventRegistry = new HandleRegistry();
 
-setInterval(runGarbageCollector, 30000);
-
-function runGarbageCollector() {
-    let kept = 0;
-    let purged = 0;
-    for (let handle in elementRegistry.registry) {
-        const element = elementRegistry.registry[handle];
-        if (window.document.contains(element)) {
-            kept++;
-        } else {
-            delete elementRegistry.registry[handle];
-            purged++;
-        }
+function getElementByHandle(handle) {
+    let element = elementRegistry.registry[handle];
+    if (element != null) {
+        return element;
     }
-    console.log("dom gc: k:" + kept + " p:" + purged);
+    element = document.getElementById("id-" + handle.toString(16));
+    if (element != null) {
+        return element;
+    }
+    throw new Error("bogus element handle: 0x" + handle.toString(16));
+}
+
+function getElementHandle(element) {
+    let handle_str = element.getAttribute("data-handle");
+    if (handle_str != null) {
+        return parseInt(handle_str, 16);
+    }
+    if (element.getAttribute("id") != null) {
+        // Don't clobber the hard-coded id. Use the registry to find this element later.
+        const {handle} = elementRegistry.alloc(element);
+        handle_str = handle.toString(16);
+        element.setAttribute("data-handle", handle_str);
+        return handle;
+    } else {
+        // Give the element an id to use to find it later.
+        // This avoids pointers to deleted elements preventing them from being garbaged collected.
+        const handle = elementRegistry.nextHandle();
+        handle_str = handle.toString(16);
+        element.setAttribute("id", "id-" + handle_str);
+        element.setAttribute("data-handle", handle_str);
+        return handle;
+    }
 }
 
 function getElementById(id) {
     const element = document.getElementById(id);
     if (element == null) throw new Error("no such element: " + id);
-    const {handle} = elementRegistry.alloc(element);
-    return handle;
+    return getElementHandle(element);
 }
 
 function setElementShown(handle, shown) {
-    elementRegistry.registry[handle].style.display = shown ? "" : "none";
+    getElementByHandle(handle).style.display = shown ? "" : "none";
 }
 
 function setElementTextContent(handle, text) {
-    elementRegistry.registry[handle].textContent = text;
+    getElementByHandle(handle).textContent = text;
 }
 
 function getChildrenCount(handle) {
-    return elementRegistry.registry[handle].children.length;
+    return getElementByHandle(handle).children.length;
 }
 
 function getChild(parent_handle, i) {
-    const parent = elementRegistry.registry[parent_handle];
+    const parent = getElementByHandle(parent_handle);
     const child = parent.children[i];
     if (child == null) throw new Error("bad child index: " + i);
-    const {handle} = elementRegistry.alloc(child);
-    return handle;
+    return getElementHandle(child);
 }
 
 function insertAdjacentHTML(handle, position, html) {
-    elementRegistry.registry[handle].insertAdjacentHTML(position, html);
+    getElementByHandle(handle).insertAdjacentHTML(position, html);
 }
 
 function removeLastChild(handle) {
-    const element = elementRegistry.registry[handle];
+    const element = getElementByHandle(handle);
     element.removeChild(element.lastChild);
 }
 
 function addClass(handle, class_) {
-    elementRegistry.registry[handle].classList.add(class_);
+    getElementByHandle(handle).classList.add(class_);
 }
 function removeClass(handle, class_) {
-    elementRegistry.registry[handle].classList.remove(class_);
+    getElementByHandle(handle).classList.remove(class_);
 }
 function setAttribute(handle, key, value) {
-    elementRegistry.registry[handle].setAttribute(key, value);
+    getElementByHandle(handle).setAttribute(key, value);
 }
 function getAttribute(handle, key) {
-    return elementRegistry.registry[handle].getAttribute(key);
+    return getElementByHandle(handle).getAttribute(key);
 }
 
 // Searches the start_handle node and its .parentNode ancestors for a node whose .classList.contains(class_).
 // If not found, returns stop_handle.
 function searchAncestorsForClass(start_handle, stop_handle, class_) {
-    let node = elementRegistry.registry[start_handle];
+    let node = getElementByHandle(start_handle);
     if (node.classList.contains(class_)) return start_handle;
-    const root = elementRegistry.registry[stop_handle];
+    const root = getElementByHandle(stop_handle);
     if (!root.contains(node)) return stop_handle;
     if (node === root) return stop_handle;
 
@@ -83,14 +98,13 @@ function searchAncestorsForClass(start_handle, stop_handle, class_) {
         node = node.parentNode;
         if (node === root) return stop_handle;
         if (node.classList.contains(class_)) {
-            const {handle} = elementRegistry.alloc(node);
-            return handle;
+            return getElementHandle(node);
         }
     }
 }
 
 function addEventListener(handle, event_type, cb) {
-    elementRegistry.registry[handle].addEventListener(event_type, function(event) {
+    getElementByHandle(handle).addEventListener(event_type, function(event) {
         const {handle, dispose} = eventRegistry.alloc(event);
         try {
             cb(handle);
@@ -103,8 +117,7 @@ function addEventListener(handle, event_type, cb) {
 function getEventTarget(event_handle) {
     const event = eventRegistry.registry[event_handle];
     const target = event.target;
-    const {handle} = elementRegistry.alloc(target);
-    return handle;
+    return getElementHandle(target);
 }
 
 return {

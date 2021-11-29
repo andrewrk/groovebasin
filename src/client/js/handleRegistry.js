8db0e17cdef7f2bdb68fd4e4a911d1a535c6a988
Josh Wolfe <thejoshwolfe@gmail.com>
2021-11-29 00:27:19 +0000

    don't even need a garbage collector for the dom registry

--- src/client/js/handleRegistry.js
+++ src/client/js/handleRegistry.js
@@ -2,11 +2,17 @@
 class HandleRegistry {
     constructor() {
         this.registry = {};
-        this.nextHandle = 0;
+        this._nextHandle = 0;
+    }
+    nextHandle() {
+        while (true) {
+            let handle = this._nextHandle;
+            this._nextHandle = 0x7fffffff & (this._nextHandle + 1);
+            if (!(handle in this.registry)) return handle;
+        }
     }
     alloc(obj) {
-        const handle = this.nextHandle;
-        this.nextHandle = 0x7fffffff & (this.nextHandle + 1);
+        const handle = this.nextHandle();
         this.registry[handle] = obj;
         return {
             handle,
